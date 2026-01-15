1c035ccdb3898a6c328234be047e63eef3eb2a14
Diego Sousa Silva <sousadiegos.7@@gmail.com>
2026-01-15 10:22:31 -0300

    feat: implementa funcionalidade de exclusão de tarefas

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
-def criar_tarefa(titulo, status="Pendente"):
+def criar_tarefa(id, titulo, status="Pendente"):
     if not titulo:
         return "Erro: Título vazio"
-    return {"titulo": titulo, "status": status}
+    return {"id": id, "titulo": titulo, "status": status}
+
+def deletar_tarefa(tarefas, id_tarefa):
+    for tarefa in tarefas:
+        if tarefa.get('id') == id_tarefa:
+            tarefas.remove(tarefa)
+            return True
+    return False
 
 # Simulação de uso
-tarefa1 = criar_tarefa("Entrega Logística Expressa")
-print(tarefa1)
+
 print("Sistema TechFlow Iniciado")
+tarefa1 = criar_tarefa(1, "Entrega Logística")
+lista = [tarefa1]
+print(lista)
