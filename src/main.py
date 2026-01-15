300f6d58eb1a18b9d299ce17c74ab6eca4ef22e5
Diego Sousa Silva <sousadiegos.7@@gmail.com>
2026-01-15 11:14:13 -0300

    Mudanca de pasta

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 def criar_tarefa(id, titulo, prioridade="Baixa"):
     return {"id": id, "titulo": titulo, "prioridade": prioridade, "status": "Pendente"}
 
+# Simulação de uso
 def deletar_tarefa(tarefas, id_tarefa):
     for tarefa in tarefas:
         if tarefa.get('id') == id_tarefa:
             tarefas.remove(tarefa)
             return True
     return False
-
-# Simulação de uso
 
 print("Sistema TechFlow Iniciado")
 tarefa1 = criar_tarefa(1, "Entrega Logística")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,18 @@
-from src.main import criar_tarefa # Garanta que esta linha está no topo
+import sys
+import os
+
+# Adiciona a pasta raiz ao caminho do Python para ele achar o 'src'
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.main import criar_tarefa, deletar_tarefa
 
 def test_criar_tarefa_com_prioridade():
     tarefa = criar_tarefa(1, "Entrega Urgente", "Alta")
     assert tarefa['prioridade'] == "Alta"
+    assert tarefa['titulo'] == "Entrega Urgente"
 
 def test_deletar_tarefa():
-    # Cria uma lista simulada
     tarefas = [{'id': 1, 'titulo': 'Teste'}]
-    
-    # Tenta deletar a tarefa de ID 1
     resultado = deletar_tarefa(tarefas, 1)
-    
-    # Verifica se funcionou
     assert resultado == True
     assert len(tarefas) == 0
